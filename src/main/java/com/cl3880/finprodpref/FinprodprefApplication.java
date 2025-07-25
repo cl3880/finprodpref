88d9e6359e381b26181448b8e5d2c30f145914aa
cl3880 <cl3880@nyu.edu>
2025-07-25 13:22:12 +0800

    chore: initial commit – project skeleton + Postgres Docker Compose

---
+++ src/main/java/com/cl3880/finprodpref/FinprodprefApplication.java
@@ -0,0 +1,13 @@
+package com.cl3880.finprodpref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinprodprefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinprodprefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl3880/finprodpref/FinprodprefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl3880.finprodpref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinprodprefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
